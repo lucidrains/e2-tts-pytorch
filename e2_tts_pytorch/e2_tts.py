af13a112f855dcc382a8f4c15217e3827368ce0d
lucidrains <lucidrains@gmail.com>
2024-07-11 12:34:26 -0700

    first do the bare minimum encoder

--- e2_tts_pytorch/e2_tts.py
+++ e2_tts_pytorch/e2_tts.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
-from torch.nn import Module, ModuleList
 import torch.nn.functional as F
+from torch.nn import Module, ModuleList
 
 import einx
 from einops import einsum, rearrange
@@ -17,14 +17,44 @@ from x_transformers import (
 def exists(v):
     return v is not None
 
+def default(v, d):
+    return v if exists(v) else d
+
+def divisible_by(num, den):
+    return (num % den) == 0
+
 # main class
 
 class E2TTS(Module):
     def __init__(
         self,
-        dim
+        *,
+        dim,
+        depth,
+        attn_kwargs: dict = dict(),
+        ff_kwargs: dict = dict()
     ):
         super().__init__()
+        assert divisible_by(depth, 2), 'depth needs to be even'
 
-    def forward(self, x):
+        self.layers = ModuleList([])
+
+        for _ in range(depth):
+            attn = Attention(dim = dim, **attn_kwargs)
+            ff = FeedForward(dim = dim, **ff_kwargs)
+
+            self.layers.append(ModuleList([
+                attn,
+                ff
+            ]))
+
+    def forward(
+        self,
+        x
+    ):
+
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+
         return x
