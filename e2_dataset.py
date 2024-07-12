23849ea6f6bc5c8b05e1394a8baa0cc3f298b44a
Ubuntu <azureuser@TrainingA100.rqsg30cixxqu5mkwjczmod0fvb.bx.internal.cloudapp.net>
2024-07-12 19:00:16 +0000

    use einops

--- e2_dataset.py
+++ e2_dataset.py
@@ -5,7 +5,10 @@ from utils.compute_mel import TorchMelSpectrogram
 from datasets import load_dataset
 from tokenizers import Tokenizer
 import logging
+from einops import rearrange, reduce
+
 logger = logging.getLogger(__name__)
+
 class E2EDataset(Dataset):
     def __init__(self, hf_dataset, tokenizer_path):
         self.data = load_dataset(hf_dataset, split='train')
@@ -13,9 +16,10 @@ class E2EDataset(Dataset):
         self.target_sample_rate = 22050
         self.hop_length = 256
         self.mel_spectrogram = TorchMelSpectrogram(sampling_rate=self.target_sample_rate)
+
     def __len__(self):
         return len(self.data)
-
+    
     def __getitem__(self, index):
         row = self.data[index]
         audio = row['audio']['array']
@@ -31,8 +35,14 @@ class E2EDataset(Dataset):
         
         if sample_rate != self.target_sample_rate:
             resampler = torchaudio.transforms.Resample(sample_rate, self.target_sample_rate)
-            audio_tensor = resampler(audio_tensor).unsqueeze(0)
+            audio_tensor = resampler(audio_tensor)
+        
+        audio_tensor = rearrange(audio_tensor, 't -> 1 t')
+        
         mel_spec = self.mel_spectrogram(audio_tensor)
+        
+        mel_spec = rearrange(mel_spec, '1 d t -> d t')
+        
         text = row['transcript']
         text = text.replace(" ", "[SPACE]")
         text_tokens = self.tokenize_text(text)
@@ -41,6 +51,7 @@ class E2EDataset(Dataset):
             'mel_spec': mel_spec,
             'text': text_tokens,
         }
+
     def tokenize_text(self, text):
         output = self.tokenizer.encode(text)
         return output.ids
