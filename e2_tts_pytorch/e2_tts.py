fc3af67012a317918873fa935f62b0ada3f78a5a
Phil Wang <lucidrains@gmail.com>
2024-12-03 05:32:26 -0800

    update for tweaks to laser

--- e2_tts_pytorch/e2_tts.py
+++ e2_tts_pytorch/e2_tts.py
@@ -501,6 +501,7 @@ class Transformer(Module):
         dropout = 0.1,
         num_registers = 32,
         attn_laser = False,
+        attn_laser_softclamp_value = 15.,
         attn_kwargs: dict = dict(
             gate_value_heads = True,
             softclamp_logits = True,
@@ -571,7 +572,7 @@ class Transformer(Module):
             speech_conv = DepthwiseConv(dim, kernel_size = kernel_size)
 
             attn_norm = rmsnorm_klass(dim)
-            attn = Attention(dim = dim, heads = heads, dim_head = dim_head, dropout = dropout, learned_value_residual_mix = not is_first_block, laser = attn_laser, **attn_kwargs)
+            attn = Attention(dim = dim, heads = heads, dim_head = dim_head, dropout = dropout, learned_value_residual_mix = not is_first_block, laser = attn_laser, laser_softclamp_value = attn_laser_softclamp_value, **attn_kwargs)
             attn_adaln_zero = postbranch_klass()
 
             ff_norm = rmsnorm_klass(dim)
@@ -599,7 +600,7 @@ class Transformer(Module):
                 text_conv = DepthwiseConv(dim_text, kernel_size = kernel_size)
 
                 text_attn_norm = RMSNorm(dim_text)
-                text_attn = Attention(dim = dim_text, heads = text_heads, dim_head = text_dim_head, dropout = dropout, learned_value_residual_mix = not is_first_block, laser = attn_laser, **attn_kwargs)
+                text_attn = Attention(dim = dim_text, heads = text_heads, dim_head = text_dim_head, dropout = dropout, learned_value_residual_mix = not is_first_block, laser = attn_laser, laser_softclamp_value = attn_laser_softclamp_value, **attn_kwargs)
 
                 text_ff_norm = RMSNorm(dim_text)
                 text_ff = FeedForward(dim = dim_text, glu = True, mult = text_ff_mult, dropout = dropout, **ff_kwargs)
